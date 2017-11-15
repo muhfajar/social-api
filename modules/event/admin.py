2ff1e80671080f36baa3140aba69bbc9ae0ec467
Muhamad Fajar <mfajar0910@gmail.com>
2017-11-15 11:17:43 +0700

    Add permission by user login to admin panel

--- modules/event/admin.py
+++ modules/event/admin.py
@@ -12,14 +12,19 @@ class VenueAdmin(admin.ModelAdmin):
         (None, {'fields': ('name', 'capacity', 'image')}),
         (_('Location'), {'fields': ('city', 'location',)}),
     )
-    list_display = ('name', 'capacity', 'city')
+    list_display = ('name', 'capacity', 'city', 'owner')
     search_fields = ('name', 'city')
     ordering = ('name', 'city')
     list_filter = ('city',)
 
+    def get_queryset(self, request):
+        qs = super(VenueAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(owner=request.user)
+
     def save_model(self, request, obj, form, change):
-        if getattr(obj, 'author', None) is None:
-            obj.author = request.user
+        obj.owner = request.user
         obj.save()
 
 
@@ -28,13 +33,18 @@ class EventTypeAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {'fields': ('name',)}),
     )
-    list_display = ('name',)
+    list_display = ('name', 'owner')
     search_fields = ('name',)
     ordering = ('name',)
 
+    def get_queryset(self, request):
+        qs = super(EventTypeAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(owner=request.user)
+
     def save_model(self, request, obj, form, change):
-        if getattr(obj, 'author', None) is None:
-            obj.author = request.user
+        obj.owner = request.user
         obj.save()
 
 
@@ -44,27 +54,37 @@ class EventAdmin(admin.ModelAdmin):
         (None, {'fields': ('name', 'type', 'image')}),
         (_('Detail'), {'fields': ('venue', 'date', 'open_gate')}),
     )
-    list_display = ('name', 'type', 'venue', 'date', 'open_gate')
+    list_display = ('name', 'type', 'venue', 'date', 'open_gate', 'owner')
     search_fields = ('name', 'type', 'venue', 'date', 'open_gate')
     ordering = ('name', 'type', 'venue', 'date', 'open_gate')
     list_filter = ('venue', 'type', 'date')
 
+    def get_queryset(self, request):
+        qs = super(EventAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(owner=request.user)
+
     def save_model(self, request, obj, form, change):
-        if getattr(obj, 'author', None) is None:
-            obj.author = request.user
+        obj.owner = request.user
         obj.save()
 
 
 @admin.register(Ticket)
-class EventTypeAdmin(admin.ModelAdmin):
+class TicketAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {'fields': ('name', 'event', 'price', 'image')}),
     )
-    list_display = ('name', 'event', 'price')
+    list_display = ('name', 'event', 'price', 'owner')
     search_fields = ('name', 'event', 'price')
     ordering = ('name', 'event', 'price')
 
+    def get_queryset(self, request):
+        qs = super(TicketAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(owner=request.user)
+
     def save_model(self, request, obj, form, change):
-        if getattr(obj, 'author', None) is None:
-            obj.author = request.user
+        obj.owner = request.user
         obj.save()
